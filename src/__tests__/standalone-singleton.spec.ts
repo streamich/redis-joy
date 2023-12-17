f2b2929d27ef3c274335c36a65dff2086c13cbe7
streamich <streamich@gmail.com>
2023-12-17 15:27:48 +0100

    test: 💍 add "standalone" tests

---
+++ src/__tests__/standalone-singleton.spec.ts
@@ -0,0 +1,34 @@
+import * as net from 'net';
+import {RedisClient} from '../node/RedisClient';
+import {ClusterTestSetup} from './types';
+import * as commands from './commands';
+import {ReconnectingSocket} from '../node';
+import {RespEncoder} from 'json-joy/es2020/json-pack/resp/RespEncoder';
+import {RespStreamingDecoder} from 'json-joy/es2020/json-pack/resp/RespStreamingDecoder';
+
+const host = '127.0.0.1';
+const port = 6379;
+const client = new RedisClient({
+  socket: new ReconnectingSocket({
+    createSocket: () => net.connect({host, port}),
+  }),
+  encoder: new RespEncoder(),
+  decoder: new RespStreamingDecoder(),
+});
+client.onError.listen((err) => {
+  console.error('onError', err);
+});
+client.start();
+
+const setupCluster: ClusterTestSetup = async () => {
+  await client.whenReady;
+  return {client};
+};
+
+describe('standalone (singleton client)', () => {
+  commands.run(setupCluster);
+});
+
+afterAll(() => {
+  client.stop();
+});
---
+++ src/__tests__/standalone.spec.ts
@@ -0,0 +1,36 @@
+import * as net from 'net';
+import {RedisClient} from '../node/RedisClient';
+import {ClusterTestSetup} from './types';
+import * as commands from './commands';
+import {ReconnectingSocket} from '../node';
+import {RespEncoder} from 'json-joy/es2020/json-pack/resp/RespEncoder';
+import {RespStreamingDecoder} from 'json-joy/es2020/json-pack/resp/RespStreamingDecoder';
+
+const host = '127.0.0.1';
+const port = 6379;
+const clients: RedisClient[] = [];
+
+const setupCluster: ClusterTestSetup = async () => {
+  const client = new RedisClient({
+    socket: new ReconnectingSocket({
+      createSocket: () => net.connect({host, port}),
+    }),
+    encoder: new RespEncoder(),
+    decoder: new RespStreamingDecoder(),
+  });
+  client.onError.listen((err) => {
+    console.error('onError', err);
+  });
+  client.start();  
+  clients.push(client);
+  await client.whenReady;
+  return {client};
+};
+
+describe('standalone (client per test)', () => {
+  commands.run(setupCluster);
+});
+
+afterAll(() => {
+  for (const client of clients) client.stop();
+});
