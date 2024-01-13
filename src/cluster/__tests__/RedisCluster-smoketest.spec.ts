bc27008a3b660ac25af5cdfbf477de718dd4a690
streamich <streamich@gmail.com>
2024-01-13 15:04:10 +0100

    test: 💍 do not execute cluster tests in CI

--- src/cluster/__tests__/RedisCluster-smoketest.spec.ts
+++ src/cluster/__tests__/RedisCluster-smoketest.spec.ts
@@ -10,31 +10,35 @@ const setup = () => {
 let keyCnt = 1;
 const getKey = () => 'redis-cluster-smoketest-foo' + keyCnt++;
 
-test('can set a key', async () => {
-  const {cluster} = setup();
-  cluster.start();
-  const key = getKey();
-  await cluster.cmd(['SET', key, 'bar']);
-  cluster.stop();
-});
-
-test('can set and get a key using master client', async () => {
-  const {cluster} = setup();
-  cluster.start();
-  const key = getKey();
-  const client = await cluster.getMasterClientForKey(key);
-  await client.cmd(['SET', key, 'bar']);
-  const res = await client.cmd(['GET', key], {utf8Res: true});
-  expect(res).toBe('bar');
-  cluster.stop();
-});
-
-test('can set and get a key', async () => {
-  const {cluster} = setup();
-  cluster.start();
-  const key = getKey();
-  await cluster.cmd(['SET', key, 'bar']);
-  const res = await cluster.cmd(['GET', key], {utf8Res: true});
-  expect(res).toBe('bar');
-  cluster.stop();
-});
+if (process.env.TEST_LOCAL_CLUSTER) {
+  test('can set a key', async () => {
+    const {cluster} = setup();
+    cluster.start();
+    const key = getKey();
+    await cluster.cmd(['SET', key, 'bar']);
+    cluster.stop();
+  });
+  
+  test('can set and get a key using master client', async () => {
+    const {cluster} = setup();
+    cluster.start();
+    const key = getKey();
+    const client = await cluster.getMasterClientForKey(key);
+    await client.cmd(['SET', key, 'bar']);
+    const res = await client.cmd(['GET', key], {utf8Res: true});
+    expect(res).toBe('bar');
+    cluster.stop();
+  });
+  
+  test('can set and get a key', async () => {
+    const {cluster} = setup();
+    cluster.start();
+    const key = getKey();
+    await cluster.cmd(['SET', key, 'bar']);
+    const res = await cluster.cmd(['GET', key], {utf8Res: true});
+    expect(res).toBe('bar');
+    cluster.stop();
+  });  
+} else {
+  test.todo('To enable cluster tests, set TEST_LOCAL_CLUSTER=1 in your environment variables.');
+}
