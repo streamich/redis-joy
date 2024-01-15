d15ab93f440666e21ce785f9b13830d2d0becf97
streamich <streamich@gmail.com>
2024-01-15 02:53:06 +0100

    test: 💍 improve testing setup

--- src/standalone/__tests__/scripts.spec.ts
+++ src/standalone/__tests__/scripts.spec.ts
@@ -4,20 +4,24 @@ import {ReconnectingSocket} from '../../util/ReconnectingSocket';
 import {StandaloneClient} from '../StandaloneClient';
 import {ScriptRegistry} from '../../ScriptRegistry';
 
-const host = config.standalone.host;
-const port = config.standalone.port;
-const scripts = new ScriptRegistry();
-const client = new StandaloneClient({
-  scripts,
-  socket: new ReconnectingSocket({
-    createSocket: () => net.connect({host, port}),
-  }),
-});
-client.start();
-
-scripts.set('hello-scripting', "return 'Hello, scripting!'");
+const setup = () => {
+  const host = config.standalone.host;
+  const port = config.standalone.port;
+  const scripts = new ScriptRegistry();
+  const client = new StandaloneClient({
+    scripts,
+    socket: new ReconnectingSocket({
+      createSocket: () => net.connect({host, port}),
+    }),
+  });
+  client.start();
+  return {client, scripts};
+};
 
 test('can run a script', async () => {
+  const {client, scripts} = setup();
+  scripts.set('hello-scripting', "return 'Hello, scripting!'");
   const res = await client.eval('hello-scripting', 0, [], [], {utf8Res: true});
   expect(res).toBe('Hello, scripting!');
+  client.stop();
 });
